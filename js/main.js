a7c5a694405c86aac9e6ea3da19d8d117a9cf3f9
Anna Neretinaя <neretinaaa@gmail.com>
2020-02-15 12:34:37 +0100

    задание 3.3

--- js/main.js
+++ js/main.js
@@ -7,6 +7,13 @@ var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditio
 var DESCRIPTIONS = ['семейный отель', 'бюджетный хостел', 'уютная квартра', 'на берегу моря', 'элитная вилла', 'загородный дом', 'королевский замок'];
 var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 
+var RESIDENCE_TYPES = {
+  flat: 'Квартира',
+  house: 'Дом',
+  bungalo: 'Бунгало',
+  palace: 'Дворец'
+};
+
 var LOCATION_MIN_X = 1;
 var LOCATION_MAX_X = 1200;
 var LOCATION_MIN_Y = 130;
@@ -89,5 +96,40 @@ var createPins = function () {
   mapPins.appendChild(fragment);
 };
 
-
 createPins();
+
+// Задание 3.3
+var cardTemplate = document.querySelector('#card').content.querySelector('.map__card');
+var clonedCard = cardTemplate.cloneNode(true);
+// создаю карточки на основе данных
+var createCard = function (cardElement) {
+  clonedCard.querySelector('.popup__avatar').src = cardElement.author.avatar;
+  clonedCard.querySelector('.popup__title').textContent = cardElement.offer.title;
+  clonedCard.querySelector('.popup__text--address').textContent = cardElement.offer.address;
+  clonedCard.querySelector('.popup__text--price').textContent = cardElement.offer.price + '₽/ночь';
+  clonedCard.querySelector('.popup__type').textContent = RESIDENCE_TYPES[cardElement.offer.type];
+  clonedCard.querySelector('.popup__text--capacity').textContent = cardElement.offer.rooms + ' комнаты для ' + cardElement.offer.guests + ' гостей';
+  clonedCard.querySelector('.popup__text--time').textContent = 'заезд после ' + cardElement.offer.checkin + ', выезд до ' + cardElement.offer.checkout;
+  clonedCard.querySelector('.popup__description').textContent = cardElement.offer.description;
+
+  var photoCards = clonedCard.querySelector('.popup__photos');
+  var photoCard = photoCards.querySelector('.popup__photo');
+  for (var i = 0; i < OFFERS_COUNT; i++) {
+    photoCard.src = cardElement.offer.photos[i];
+    photoCards.appendChild(photoCard);
+  }
+  return clonedCard;
+};
+
+var map = document.querySelector('.map');
+var mapFilters = map.querySelector('.map__filters-container');
+
+var createCards = function () {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < OFFERS_COUNT; i++) {
+    fragment.appendChild(createPin(createCard[i]));
+  }
+  map.insertBefore(fragment, mapFilters);
+};
+
+createCards();
